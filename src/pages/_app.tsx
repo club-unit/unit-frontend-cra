947416db3892c08b5af3785f66fc8ef992c69a46
Yikanghoon <hoon5083@gmail.com>
2023-09-09 04:22:41 +0900

    feat: add swr config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,8 @@ import { MOCKUP_BOARDS } from "@/mockups/boards";
 import { AuthProvider } from "@/contexts/auth/AuthProvider";
 import AuthOrUserCard from "@/components/common/AuthOrUserCard";
 import { NotificationProvider } from "@/contexts/notification/NotificationProvider";
+import { SWRConfig } from "swr";
+import { fetcher } from "@/utils/fetcher";
 
 export default function App({ Component, pageProps }: AppProps) {
   const boardsResponse = MOCKUP_BOARDS;
@@ -19,29 +21,36 @@ export default function App({ Component, pageProps }: AppProps) {
   }));
 
   return (
-    <NotificationProvider>
-      <AuthProvider>
-        <Layout>
-          <Layout.Header>
-            <Menu
-              items={menuItems}
-              mode="horizontal"
-              className="bg-transparent text-white text-lg font-bold"
-            />
-          </Layout.Header>
-          <Layout.Content className="flex justify-center">
-            <Row className="p-3 max-w-7xl min-h-screen mx-0" gutter={16}>
-              <Col span={18}>
-                <Component {...pageProps} />
-              </Col>
-              <Col span={6} className="pt-2">
-                <AuthOrUserCard />
-              </Col>
-            </Row>
-          </Layout.Content>
-          <Layout.Footer></Layout.Footer>
-        </Layout>
-      </AuthProvider>
-    </NotificationProvider>
+    <SWRConfig
+      value={{
+        fetcher: fetcher,
+        shouldRetryOnError: false,
+      }}
+    >
+      <NotificationProvider>
+        <AuthProvider>
+          <Layout>
+            <Layout.Header>
+              <Menu
+                items={menuItems}
+                mode="horizontal"
+                className="bg-transparent text-white text-lg font-bold"
+              />
+            </Layout.Header>
+            <Layout.Content className="flex justify-center">
+              <Row className="p-3 max-w-7xl min-h-screen mx-0" gutter={16}>
+                <Col span={18}>
+                  <Component {...pageProps} />
+                </Col>
+                <Col span={6} className="pt-2">
+                  <AuthOrUserCard />
+                </Col>
+              </Row>
+            </Layout.Content>
+            <Layout.Footer></Layout.Footer>
+          </Layout>
+        </AuthProvider>
+      </NotificationProvider>
+    </SWRConfig>
   );
 }
