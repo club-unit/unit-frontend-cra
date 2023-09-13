70a6b2e55b2b0f85afb3147bbddad1cb7c6d47f9
Yikanghoon <hoon5083@gmail.com>
2023-09-14 06:36:38 +0900

    feat: move static files and mod config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import "@/styles/globals.css";
+import "@/globals.css";
 import type { AppProps } from "next/app";
 import { Col, Layout, Row } from "antd";
 import { AuthProvider } from "@/contexts/auth/AuthProvider";
