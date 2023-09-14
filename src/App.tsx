abcef5a3ad11fc67c4669cfc461ad3fbe8957e18
Yikanghoon <hoon5083@gmail.com>
2023-09-15 04:47:52 +0900

    feat: cra migration

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,11 @@ import { Col, Layout, Row } from "antd";
 import MainPage from "src/pages/MainPage";
 import AuthOrUserCard from "src/components/common/AuthOrUserCard";
 import Navbar from "src/components/common/Navbar";
+import { Route, Routes } from "react-router-dom";
+import PasswordResetPage from "src/pages/pw-reset";
+import RegisterPage from "src/pages/register";
+import PostListPage from "src/pages/[slug]";
+import PostPage from "src/pages/[slug]/[Id]";
 
 function App() {
   return (
@@ -13,7 +18,15 @@ function App() {
       <Layout.Content className="flex justify-center">
         <Row className="p-3 w-[80vw] min-h-screen mx-0" gutter={16}>
           <Col span={18}>
-            <MainPage />
+            <Routes>
+              <Route path="/" element={<MainPage />} />
+              <Route path="/pw-reset" element={<PasswordResetPage />} />
+              <Route path="/register" element={<RegisterPage />} />
+              <Route path="/:slug">
+                <Route path="" element={<PostListPage />} />
+                <Route path=":id" element={<PostPage />} />
+              </Route>
+            </Routes>
           </Col>
           <Col span={6} className="pt-2">
             <AuthOrUserCard />
--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,8 @@ import { fetcher } from "./utils/fetcher";
 import { SWRConfig } from "swr";
 import NotificationProvider from "src/contexts/notification/NotificationProvider";
 import AuthProvider from "src/contexts/auth/AuthProvider";
-import { Col, Layout, Row } from "antd";
-import Navbar from "src/components/common/Navbar";
-import MainPage from "src/pages/MainPage";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import PasswordResetPage from "src/pages/pw-reset";
-import RegisterPage from "src/pages/register";
-import PostPage from "src/pages/[slug]/[Id]";
-import PostListPage from "src/pages/[slug]";
-import AuthOrUserCard from "src/components/common/AuthOrUserCard";
+import { BrowserRouter } from "react-router-dom";
+import App from "src/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
@@ -27,30 +20,7 @@ root.render(
       <BrowserRouter>
         <NotificationProvider>
           <AuthProvider>
-            <Layout>
-              <Layout.Header>
-                <Navbar />
-              </Layout.Header>
-              <Layout.Content className="flex justify-center">
-                <Row className="p-3 w-[80vw] min-h-screen mx-0" gutter={16}>
-                  <Col span={18}>
-                    <Routes>
-                      <Route path="/" element={<MainPage />} />
-                      <Route path="/pw-reset" element={<PasswordResetPage />} />
-                      <Route path="/register" element={<RegisterPage />} />
-                      <Route path="/:slug">
-                        <Route path="" element={<PostListPage />} />
-                        <Route path=":id" element={<PostPage />} />
-                      </Route>
-                    </Routes>
-                  </Col>
-                  <Col span={6} className="pt-2">
-                    <AuthOrUserCard />
-                  </Col>
-                </Row>
-              </Layout.Content>
-              <Layout.Footer></Layout.Footer>
-            </Layout>
+            <App />
           </AuthProvider>
         </NotificationProvider>
       </BrowserRouter>
