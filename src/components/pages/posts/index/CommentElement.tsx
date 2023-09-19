1af3029b3a9aab1e347cff4dccc87a0def57f653
Yikanghoon <hoon5083@gmail.com>
2023-09-20 08:36:12 +0900

    feat: 댓글 요소 레이아웃 변경

--- src/components/pages/posts/index/CommentElement.tsx
+++ src/components/pages/posts/index/CommentElement.tsx
@@ -1,5 +1,7 @@
-import { Typography } from "antd";
+import { Button, Image, Typography } from "antd";
 import { Comment } from "src/types/api/comment";
+import { ClockCircleOutlined } from "@ant-design/icons";
+import dayjs from "dayjs";
 
 interface Props {
   comment: Comment;
@@ -8,9 +10,42 @@ interface Props {
 function CommentElement({ comment }: Props) {
   return (
     <div className="flex flex-col border-y-2 py-2 px-4 items-start w-full gap-2">
-      <div>
-        <Typography.Text strong>{comment.author.profile.name}</Typography.Text>
+      <div className="flex justify-between w-full">
+        <div className="flex gap-1">
+          <Image
+            height={20}
+            src={`/icons/rank/${comment.author.profile.rank}.png`}
+            alt={String(comment.author.profile.rank)}
+            preview={false}
+          />
+          {comment.author.profile.responsibility !== "NONE" &&
+            comment.author.profile.responsibility !== "NORMAL" && (
+              <Image
+                height={20}
+                src={`/icons/responsibility/${comment.author.profile.responsibility}.png`}
+                alt={String(comment.author?.profile.responsibility)}
+                preview={false}
+              />
+            )}
+          <Typography.Text>{comment.author.profile.name}</Typography.Text>
+          <div className="flex gap-2 ml-4">
+            <ClockCircleOutlined />
+            <Typography.Text>{dayjs(comment.created).format("MM/DD hh:mm")}</Typography.Text>
+          </div>
+        </div>
+        <div className="flex gap-2">
+          <Button size="small" type="text">
+            답글
+          </Button>
+          <Button size="small" type="text">
+            수정
+          </Button>
+          <Button size="small" type="text">
+            삭제
+          </Button>
+        </div>
       </div>
+
       <Typography.Text>{comment.content}</Typography.Text>
     </div>
   );
