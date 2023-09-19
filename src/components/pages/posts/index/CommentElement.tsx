807721d57c1a07f6451ab39868a715ce255d993d
Yikanghoon <hoon5083@gmail.com>
2023-09-20 08:47:22 +0900

    feat: comment child 레이아웃 추가

--- src/components/pages/posts/index/CommentElement.tsx
+++ src/components/pages/posts/index/CommentElement.tsx
@@ -5,48 +5,53 @@ import dayjs from "dayjs";
 
 interface Props {
   comment: Comment;
+  isChildren?: boolean;
 }
 
-function CommentElement({ comment }: Props) {
+function CommentElement({ comment, isChildren }: Props) {
   return (
-    <div className="flex flex-col border-y-2 py-2 px-4 items-start w-full gap-2">
-      <div className="flex justify-between w-full">
-        <div className="flex gap-1">
-          <Image
-            height={20}
-            src={`/icons/rank/${comment.author.profile.rank}.png`}
-            alt={String(comment.author.profile.rank)}
-            preview={false}
-          />
-          {comment.author.profile.responsibility !== "NONE" &&
-            comment.author.profile.responsibility !== "NORMAL" && (
-              <Image
-                height={20}
-                src={`/icons/responsibility/${comment.author.profile.responsibility}.png`}
-                alt={String(comment.author?.profile.responsibility)}
-                preview={false}
-              />
-            )}
-          <Typography.Text>{comment.author.profile.name}</Typography.Text>
-          <div className="flex gap-2 ml-4">
-            <ClockCircleOutlined />
-            <Typography.Text>{dayjs(comment.created).format("MM/DD hh:mm")}</Typography.Text>
+    <div className={`${isChildren ? "w-11/12" : "w-full"} items-end border-t-2 ml-auto`}>
+      <div className="flex flex-col py-2 px-4 w-full gap-2">
+        <div className="flex justify-between w-full">
+          <div className="flex gap-1">
+            <Image
+              height={20}
+              src={`/icons/rank/${comment.author.profile.rank}.png`}
+              alt={String(comment.author.profile.rank)}
+              preview={false}
+            />
+            {comment.author.profile.responsibility !== "NONE" &&
+              comment.author.profile.responsibility !== "NORMAL" && (
+                <Image
+                  height={20}
+                  src={`/icons/responsibility/${comment.author.profile.responsibility}.png`}
+                  alt={String(comment.author?.profile.responsibility)}
+                  preview={false}
+                />
+              )}
+            <Typography.Text>{comment.author.profile.name}</Typography.Text>
+            <div className="flex gap-2 ml-4">
+              <ClockCircleOutlined />
+              <Typography.Text>{dayjs(comment.created).format("MM/DD hh:mm")}</Typography.Text>
+            </div>
+          </div>
+          <div className="flex gap-2">
+            <Button size="small" type="text">
+              답글
+            </Button>
+            <Button size="small" type="text">
+              수정
+            </Button>
+            <Button size="small" type="text">
+              삭제
+            </Button>
           </div>
         </div>
-        <div className="flex gap-2">
-          <Button size="small" type="text">
-            답글
-          </Button>
-          <Button size="small" type="text">
-            수정
-          </Button>
-          <Button size="small" type="text">
-            삭제
-          </Button>
-        </div>
+        <Typography.Text>{comment.content}</Typography.Text>
       </div>
-
-      <Typography.Text>{comment.content}</Typography.Text>
+      {comment.children.length
+        ? comment.children.map((child) => <CommentElement comment={child} isChildren />)
+        : null}
     </div>
   );
 }
