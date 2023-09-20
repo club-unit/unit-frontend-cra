6c4e9b0ecc5d5da846aa4bdf0c5a070a2e346357
Yikanghoon <hoon5083@gmail.com>
2023-09-20 13:49:41 +0900

    feat: 댓글 삭제 API 연결

--- src/components/pages/posts/index/CommentElement.tsx
+++ src/components/pages/posts/index/CommentElement.tsx
@@ -4,6 +4,10 @@ import { ClockCircleOutlined } from "@ant-design/icons";
 import dayjs from "dayjs";
 import { Dispatch } from "react";
 import CommentInput from "src/components/pages/posts/index/CommentInput";
+import { clientAxios } from "src/utils/clientAxios";
+import { API_ROUTES } from "src/constants/routes";
+import { useParams } from "react-router-dom";
+import useNotification from "src/contexts/notification/useNotfication";
 
 interface Props {
   comment: Comment;
@@ -14,6 +18,21 @@ interface Props {
 }
 
 function CommentElement({ comment, isChildren, replyingParent, setReplyingParent, mutate }: Props) {
+  const { slug, id } = useParams();
+  const { api } = useNotification();
+
+  const handleDelete = async () => {
+    try {
+      await clientAxios.delete(
+        API_ROUTES.comments.bySlugAndPostIdAndId(String(slug), Number(id), comment.id)
+      );
+      mutate();
+      api.success({ message: "댓글이 삭제되었습니다." });
+    } catch (e) {
+      api.error({ message: "댓글 삭제에 실패했습니다.", description: "다시 시도해주세요." });
+    }
+  };
+
   return (
     <div className={`${isChildren ? "w-[98%]" : "w-full"} items-end border-t-2 ml-auto`}>
       <div className="flex flex-col py-2 px-4 w-full gap-2">
@@ -41,13 +60,19 @@ function CommentElement({ comment, isChildren, replyingParent, setReplyingParent
             </div>
           </div>
           <div className="flex gap-2">
-            <Button size="small" type="text" onClick={() => setReplyingParent(comment.id)}>
+            <Button
+              size="small"
+              type="text"
+              onClick={() => {
+                setReplyingParent(replyingParent !== comment.id ? comment.id : null);
+              }}
+            >
               답글
             </Button>
             <Button size="small" type="text">
               수정
             </Button>
-            <Button size="small" type="text">
+            <Button size="small" type="text" onClick={handleDelete}>
               삭제
             </Button>
           </div>
