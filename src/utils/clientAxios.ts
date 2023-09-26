ba5aeb48ea186d1f064710da8b2f5097fe724b06
Yikanghoon <hoon5083@gmail.com>
2023-09-26 12:04:28 +0900

    fix: remove redirect on 401

--- src/utils/clientAxios.ts
+++ src/utils/clientAxios.ts
@@ -4,15 +4,15 @@ export const clientAxios = axios.create({
   baseURL: process.env.REACT_APP_API_ENDPOINT,
 });
 
-clientAxios.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  (error) => {
-    if (error.response && error.response.status === 401) {
-      alert("권한이 없습니다.");
-      window.location.replace("/");
-    }
-    return Promise.reject(error);
-  }
-);
+// clientAxios.interceptors.response.use(
+//   (response) => {
+//     return response;
+//   },
+//   (error) => {
+//     if (error.response && error.response.status === 401) {
+//       alert("권한이 없습니다.");
+//       window.location.replace("/");
+//     }
+//     return Promise.reject(error);
+//   }
+// );
