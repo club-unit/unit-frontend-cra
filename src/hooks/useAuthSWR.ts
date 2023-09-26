32a67fb4c644b876802e63a76092985859ab5473
Yikanghoon <hoon5083@gmail.com>
2023-09-26 20:16:09 +0900

    feat: mod refresh logic

--- src/hooks/useAuthSWR.ts
+++ src/hooks/useAuthSWR.ts
@@ -1,6 +1,6 @@
 import useSWR from "swr";
 import Cookies from "js-cookie";
-import { ACCESS_COOKIE_NAME } from "src/constants/jwt";
+import { ACCESS_COOKIE_NAME, REFRESH_COOKIE_NAME } from "src/constants/jwt";
 import useNotification from "src/contexts/notification/useNotfication";
 import { useNavigate } from "react-router-dom";
 import useAuth from "src/contexts/auth/useAuth";
@@ -23,12 +23,18 @@ function useAuthSWR<T>(params: Params | null) {
   } else if (error?.response?.status === 401) {
     if (error?.response?.data?.detail === "이 토큰은 모든 타입의 토큰에 대해 유효하지 않습니다") {
       refresh();
+      mutate();
     } else if (
       error?.response?.data?.detail ===
       "자격 인증데이터(authentication credentials)가 제공되지 않았습니다."
     ) {
-      navigate("/");
-      api.error({ message: "로그인이 필요합니다." });
+      if (Cookies.get(REFRESH_COOKIE_NAME)) {
+        refresh();
+        mutate();
+      } else {
+        navigate("/");
+        api.error({ message: "로그인이 필요합니다." });
+      }
     }
   }
 
