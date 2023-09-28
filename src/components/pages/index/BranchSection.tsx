453648cddeb21e1b558107a896b7f949d310cf33
Yikanghoon <hoon5083@gmail.com>
2023-09-28 23:45:46 +0900

    feat: add breakpoint on branch section

--- src/components/pages/index/BranchSection.tsx
+++ src/components/pages/index/BranchSection.tsx
@@ -2,7 +2,7 @@ import BranchCard from "src/components/pages/index/BranchCard";
 
 function BranchSection() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-2">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2">
       <BranchCard slug="green" />
       <BranchCard slug="donga" />
       <BranchCard slug="city" />
