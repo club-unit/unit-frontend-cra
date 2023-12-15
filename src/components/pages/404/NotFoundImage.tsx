3fd07100506e293a346d5b560bbb50147707a804
Yikanghoon <hoon5083@gmail.com>
2023-12-16 04:29:14 +0900

    feat: edit 404 page

--- src/components/pages/404/NotFoundImage.tsx
+++ src/components/pages/404/NotFoundImage.tsx
@@ -1,9 +1,13 @@
 import { Typography } from "antd";
+import { RobotOutlined } from "@ant-design/icons";
 
 function NotFoundImage() {
   return (
-    <div className="flex w-full h-full justify-center items-center">
-      <Typography.Title>존재하지 않는 페이지입니다</Typography.Title>
+    <div className="flex flex-col h-full justify-center items-center gap-8 px-4">
+      <div>
+        <RobotOutlined style={{ fontSize: 200 }} />
+      </div>
+      <Typography.Title className="text-center">존재하지 않는 페이지입니다</Typography.Title>
     </div>
   );
 }
