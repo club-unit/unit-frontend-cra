06c7d89a6568dc73d2bb02583c58dbf9b86b7351
Yikanghoon <hoon5083@gmail.com>
2023-12-20 04:49:08 +0900

    fix: wrong redirect problem

--- src/components/common/UserHeader.tsx
+++ src/components/common/UserHeader.tsx
@@ -4,14 +4,20 @@ import BadgeSet from "src/components/common/BadgeSet";
 import { User } from "src/types/api/user";
 import { Author } from "src/types/api/author";
 import { Link } from "react-router-dom";
+import useAuth from "src/contexts/auth/useAuth";
 
 interface Props {
   user: User | Author;
 }
 
 function UserHeader({ user }: Props) {
+  const { user: me } = useAuth();
+
   return (
-    <Link to={`/users/${user.id}`} className="hover:bg-gray-200 rounded-lg">
+    <Link
+      to={user && me && user?.id === me?.id ? `/users/me` : `/users/${user.id}`}
+      className="hover:bg-gray-200 rounded-lg"
+    >
       <div className="flex gap-2 px-1 py-1">
         <Avatar icon={<UserOutlined />} src={user.profile.profilePhoto} />
         <div className="flex gap-1 items-center">
