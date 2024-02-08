337bf4866d7fbf9ce8c7e5846e6eba09f707ae7c
Yikanghoon <hoon5083@gmail.com>
2024-02-08 20:52:09 +0900

    refactor: 유저 관련 페이지 카드 구조 수정

--- src/pages/users/[id].tsx
+++ src/pages/users/[id].tsx
@@ -1,14 +1,13 @@
-import { Avatar, Card, Descriptions, DescriptionsProps, Typography } from "antd";
-import { SEX_LOOKUP_TABLE } from "src/constants/user";
+import { Typography } from "antd";
 import { withAuth } from "src/components/common/withAuth";
-import { UserOutlined } from "@ant-design/icons";
 import useAuthSWR from "src/hooks/useAuthSWR";
 import { API_ROUTES } from "src/constants/routes";
 import { useNavigate, useParams } from "react-router-dom";
 import { OtherUser } from "src/types/api/user";
-import getActivityItems from "src/utils/users/getActivityItems";
 import useAuth from "src/contexts/auth/useAuth";
 import { useEffect } from "react";
+import ActivityInfoCard from "src/components/pages/users/ActivityInfoCard";
+import BasicInfoCard from "src/components/pages/users/BasicInfoCard";
 
 function ProfilePage() {
   const { id } = useParams();
@@ -22,26 +21,6 @@ function ProfilePage() {
       : null
   );
 
-  const othersPersonalItems: DescriptionsProps["items"] = [
-    {
-      key: "1",
-      label: "이름",
-      children: <p>{user?.profile.name}</p>,
-    },
-    {
-      key: "2",
-      label: "성별",
-      children: <p>{SEX_LOOKUP_TABLE[user?.profile.sex as number]}</p>,
-    },
-    {
-      key: "3",
-      label: "아이디",
-      children: <p>{user?.username}</p>,
-    },
-  ];
-
-  const activityItems = user ? getActivityItems(user) : undefined;
-
   useEffect(() => {
     if (user && me && user?.id === me?.id) {
       navigate("/users/me");
@@ -53,15 +32,8 @@ function ProfilePage() {
       <Typography.Title level={2}>
         {user?.profile.name}({user?.username})님의 프로필
       </Typography.Title>
-      <Card title="기본 정보">
-        <div className="flex flex-col gap-4 justify-end">
-          <Avatar icon={<UserOutlined />} src={user?.profile.profilePhoto} size={200} />
-          <Descriptions items={othersPersonalItems} />
-        </div>
-      </Card>
-      <Card title="활동 정보">
-        <Descriptions items={activityItems} />
-      </Card>
+      <BasicInfoCard otherUser={user} />
+      <ActivityInfoCard otherUser={user} />
     </div>
   );
 }
--- src/pages/users/me.tsx
+++ src/pages/users/me.tsx
@@ -1,69 +1,19 @@
-import { Avatar, Button, Card, Descriptions, Typography, Upload } from "antd";
+import { Typography } from "antd";
 import useAuth from "src/contexts/auth/useAuth";
-import { clientAxios } from "src/utils/common/clientAxios";
-import { API_ROUTES } from "src/constants/routes";
 import { withAuth } from "src/components/common/withAuth";
-import { UploadOutlined, UserOutlined } from "@ant-design/icons";
-import getActivityItems from "src/utils/users/getActivityItems";
-import getLoginItems from "src/utils/users/getLoginItems";
-import useNotification from "src/contexts/notification/useNotfication";
-import getPersonalItems from "src/utils/users/getPersonalItems";
+import BasicInfoCard from "src/components/pages/users/BasicInfoCard";
+import LoginInfoCard from "src/components/pages/users/LoginInfoCard";
+import ActivityInfoCard from "src/components/pages/users/ActivityInfoCard";
 
 function MyPage() {
   const { user } = useAuth();
-  const { api } = useNotification();
-
-  const personalItems = user ? getPersonalItems(user, true) : undefined;
-  const loginItems = user ? getLoginItems(user) : undefined;
-  const activityItems = user ? getActivityItems(user) : undefined;
-
-  const uploadProfileImage = async (options: any) => {
-    const { onSuccess, onError, file, onProgress } = options;
-    const formData = new FormData();
-    formData.append("profile_photo", file);
-
-    if (file.size > 2 * 1024 * 1024) {
-      api.error({
-        message: "이미지 크기가 너무 큽니다.",
-        description: "2MB 이하의 이미지를 선택해주세요",
-      });
-      return;
-    }
-    try {
-      const response = await clientAxios.put(API_ROUTES.users.changeProfilePhoto(), formData, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-        onUploadProgress: (event) => {
-          onProgress({ percent: (event.loaded / (event.total || 1)) * 100 });
-        },
-      });
-      onSuccess(response.data, file);
-    } catch (error) {
-      onError(error);
-    } finally {
-      window.location.reload();
-    }
-  };
 
   return (
     <div className="flex flex-col gap-2">
       <Typography.Title level={2}>마이페이지</Typography.Title>
-      <Card title="기본 정보">
-        <div className="flex flex-col gap-4 justify-end">
-          <Avatar icon={<UserOutlined />} src={user?.profile.profilePhoto} size={200} />
-          <Upload name="avatar" showUploadList={false} customRequest={uploadProfileImage}>
-            <Button icon={<UploadOutlined />}>프로필 이미지 변경하기</Button>
-          </Upload>
-          <Descriptions items={personalItems} />
-        </div>
-      </Card>
-      <Card title="로그인 정보">
-        <Descriptions items={loginItems} />
-      </Card>
-      <Card title="활동 정보">
-        <Descriptions items={activityItems} />
-      </Card>
+      <BasicInfoCard />
+      <LoginInfoCard />
+      <ActivityInfoCard />
     </div>
   );
 }
