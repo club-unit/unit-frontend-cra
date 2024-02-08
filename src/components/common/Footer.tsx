1e5b834dfcb441464efbeb6fe7a3ef6e1de6fb69
Yikanghoon <hoon5083@gmail.com>
2024-02-08 21:34:41 +0900

    feat: footer 모바일 레이아웃 대응

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -6,7 +6,7 @@ function Footer() {
   return (
     <div className="flex flex-col justify-center bg-gray-300 p-8">
       <Typography.Text style={{ fontSize: 25, fontWeight: 800 }}>UNIT</Typography.Text>
-      <div className="flex justify-between">
+      <div className="flex flex-col sm:flex-row justify-between gap-5">
         <div className="flex flex-col">
           <Typography.Text style={{ fontSize: 15, fontWeight: 300, color: "#515151" }}>
             유니트 | 대학연합레저스포츠 동아리
@@ -17,7 +17,7 @@ function Footer() {
             </a>
           </div>
         </div>
-        <div className="flex flex-col items-end">
+        <div className="flex flex-col items-start sm:items-end">
           <Typography.Text style={{ fontSize: 15, fontWeight: 300, color: "#515151" }}>
             회장: 김동현 | 010-8749-3269
           </Typography.Text>
