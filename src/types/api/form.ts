edbf7e53fb35f535bbb964447ec30482b6d27573
Yikanghoon <hoon5083@gmail.com>
2024-02-12 22:05:00 +0900

    feat: form type 변경

--- src/types/api/form.ts
+++ src/types/api/form.ts
@@ -1,21 +1,29 @@
 export interface Form {
+  id: number;
   title: string;
   description: string;
   sections: FormSection[];
 }
 
 export interface FormSection {
+  id: number;
   title: string;
   description: string;
   questions: FormQuestion[];
 }
 
 export interface FormQuestion {
+  id: number;
   content: string;
   description: string;
   type: FormQuestionType;
   isRequired: boolean;
-  options: string[];
+  options: FormQuestionOption[];
+}
+
+export interface FormQuestionOption {
+  id: number;
+  content: string;
 }
 
 export type FormQuestionType = "MULTIPLE_CHOICE" | "SHORT_ANSWER";
