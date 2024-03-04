0154ba2b93471853182e59df0880d5076339abc6
Yikanghoon <hoon5083@gmail.com>
2024-03-04 16:16:32 +0900

    feat: 지원자 통계 카드 추가

--- src/pages/applications.tsx
+++ src/pages/applications.tsx
@@ -9,6 +9,7 @@ import { Application } from "src/types/api/application";
 import { CommonListResponse } from "src/types/api/common";
 import { Spin } from "antd";
 import useAuth from "src/contexts/auth/useAuth";
+import ApplicationsStatisticsSection from "src/components/pages/applications/ApplicationsStatisticsSection";
 
 function ApplicationsPage() {
   const { user } = useAuth();
@@ -28,7 +29,14 @@ function ApplicationsPage() {
         currentBranch={currentBranch}
         setCurrentBranch={setCurrentBranch}
       />
-      {data ? <ApplicationsTableSection applications={data} mutate={mutate} /> : <Spin />}
+      {data ? (
+        <div className="mt-2">
+          <ApplicationsStatisticsSection applications={data} isOverall={currentBranch === "ALL"} />
+          <ApplicationsTableSection applications={data} mutate={mutate} />
+        </div>
+      ) : (
+        <Spin />
+      )}
     </>
   );
 }
