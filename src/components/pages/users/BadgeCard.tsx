cba5ea55fb839a92174b97e3de5aa77abd40f37b
Yikanghoon <hoon5083@gmail.com>
2024-10-13 22:44:22 +0900

    feat: 뱃지 실제 api 적용

--- src/components/pages/users/BadgeCard.tsx
+++ src/components/pages/users/BadgeCard.tsx
@@ -1,23 +1,28 @@
 import { Avatar, Card, Popover } from "antd";
 import { OtherUser } from "src/types/api/user";
-import { MOCKUP_BADGE_LIST } from "src/mockups/badge";
 import { TrophyOutlined } from "@ant-design/icons";
+import useAuth from "src/contexts/auth/useAuth";
 
 interface Props {
   otherUser?: OtherUser;
 }
 
 function BadgeCard({ otherUser }: Props) {
-  const badges = MOCKUP_BADGE_LIST;
+  const { user } = useAuth();
+  const badges = otherUser ? otherUser?.profile.badges : user?.profile.badges;
 
   return (
     <Card title="뱃지">
       <div className="flex gap-5 flex-wrap">
-        {badges.map((badge) => (
-          <Popover content={badge.description}>
-            <Avatar icon={<TrophyOutlined />} src={badge.image} size={70} />
-          </Popover>
-        ))}
+        {badges?.length ? (
+          badges?.map((badge) => (
+            <Popover content={badge.name}>
+              <Avatar icon={<TrophyOutlined />} src={badge.image} size={70} />
+            </Popover>
+          ))
+        ) : (
+          <p className="text-gray-500">뱃지가 없습니다</p>
+        )}
       </div>
     </Card>
   );
