3ec528fff0c2fd902cce7d697647c5d9a05cde54
Kanghoon YI <kanghoon.yi.dev@gmail.com>
2025-03-27 20:46:36 +0900

    feat: usePosts 추가

---
+++ src/hooks/boards/usePosts.ts
@@ -0,0 +1,28 @@
+import useAuthSWR from "src/hooks/common/useAuthSWR";
+import { API_ROUTES } from "src/constants/routes";
+import { PostSummary } from "src/types/api/post";
+import { CommonPagedResponse } from "src/types/api/common";
+
+interface PostListQuery {
+  categoryName?: string;
+  ordering?: string;
+  page?: number;
+}
+
+function usePosts(slug: string, query: PostListQuery, isDisabled: boolean) {
+  const { data, isLoading } = useAuthSWR<CommonPagedResponse<PostSummary>>(
+    !isDisabled && slug
+      ? {
+          url: API_ROUTES.posts.bySlug(slug),
+          query: {
+            category__name: query.categoryName,
+            ordering: query.ordering,
+            page: query.page,
+          },
+        }
+      : null
+  );
+  return { data, isLoading };
+}
+
+export default usePosts;
