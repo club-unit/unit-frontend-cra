28f21836225c81b55244251b5ec0c53f7b04d212
Yi Kanghoon <yikanghoon@kanghoon-MacBook-Pro.local>
2026-02-16 18:08:36 +0900

    feat(mainpage): 전지구대 게시판 메인에 추가

--- src/components/pages/index/BranchSection.tsx
+++ src/components/pages/index/BranchSection.tsx
@@ -2,17 +2,20 @@ import { useMemo } from "react";
 import BranchCard from "src/components/pages/index/BranchCard";
 import { BRANCH_SLUGS } from "src/constants/branches";
 import useAuth from "src/contexts/auth/useAuth";
+import BoardCard from "src/components/pages/index/BoardCard";
 
 function BranchSection() {
   const { user } = useAuth();
   const myBranch = user?.profile.branch?.toLowerCase();
+  const branchSlugs = ["green", "donga", "city", "union", "jamsil"];
 
   const restBranches = useMemo(() => {
-    return BRANCH_SLUGS.filter((slug) => slug !== myBranch);
+    return branchSlugs.filter((slug) => slug !== myBranch);
   }, [myBranch]);
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2">
+      <BoardCard slug="all-branch" key="all-branch" title={"전 지구대"} />
       {myBranch && <BranchCard slug={myBranch} myBranch />}
       {restBranches.map((slug) => (
         <BranchCard slug={slug} key={slug} />
